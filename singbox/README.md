1d5520b1c792e8665538ae43b8f522ac2281a958
wangzf <wlmxenl@outlook.com>
2025-06-03 14:45:49 +0800

    singbox-1.11.x

---
+++ singbox/README.md
@@ -0,0 +1,4 @@
+## 配置参考
+
+- https://github.com/xishang0128/sub-store-template
+- https://github.com/Toperlock/sing-box-subscribe
---
+++ singbox/config/script.js
@@ -0,0 +1,38 @@
+const { type, name } = $arguments
+const compatible_outbound = {
+  tag: 'COMPATIBLE',
+  type: 'direct',
+}
+
+let compatible
+let config = JSON.parse($files[0])
+let proxies = await produceArtifact({
+  name,
+  type: /^1$|col/i.test(type) ? 'collection' : 'subscription',
+  platform: 'sing-box',
+  produceType: 'internal',
+})
+
+config.outbounds.push(...proxies)
+
+config.outbounds.map(i => {
+  if (['节点选择', '自动选择'].includes(i.tag)) {
+    i.outbounds.push(...getTags(proxies))
+  }
+})
+
+config.outbounds.forEach(outbound => {
+  if (Array.isArray(outbound.outbounds) && outbound.outbounds.length === 0) {
+    if (!compatible) {
+      config.outbounds.push(compatible_outbound)
+      compatible = true
+    }
+    outbound.outbounds.push(compatible_outbound.tag);
+  }
+});
+
+$content = JSON.stringify(config, null, 2)
+
+function getTags(proxies, regex) {
+  return (regex ? proxies.filter(p => regex.test(p.tag)) : proxies).map(p => p.tag)
+}
