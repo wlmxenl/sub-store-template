1cb958f28ae417f8d689e3dec10612cd8ac85d5c
wangzf <99050625@qq.com>
2025-07-30 11:16:57 +0800

    去除 mihomo 脚本节点内部字段

--- mihomo/config/script.js
+++ mihomo/config/script.js
@@ -14,37 +14,26 @@ let clashMetaProxies = await produceArtifact({
     produceType: 'internal'
 })
 
-// // 递归删除以 "_" 开头的字段
-// function removeUnderscoreFields(obj) {
-//     if (Array.isArray(obj)) {
-//         return obj.map(removeUnderscoreFields)
-//     } else if (typeof obj === 'object' && obj !== null) {
-//         const newObj = {}
-//         for (const key in obj) {
-//             if (!key.startsWith('_')) {
-//                 newObj[key] = removeUnderscoreFields(obj[key])
-//             }
-//         }
-//         return newObj
-//     }
-//     return obj
-// }
+// 递归删除以 "_" 开头的内部字段
+function removeUnderscoreFields(obj) {
+    if (Array.isArray(obj)) {
+        return obj.map(removeUnderscoreFields)
+    } else if (typeof obj === 'object' && obj !== null) {
+        const newObj = {}
+        for (const key in obj) {
+            if (!key.startsWith('_')) {
+                newObj[key] = removeUnderscoreFields(obj[key])
+            }
+        }
+        return newObj
+    }
+    return obj
+}
 
-// clashMetaProxies = removeUnderscoreFields(clashMetaProxies)
-
-// // 给每个代理节点的名字加上序号，格式：01 - 原名
-// clashMetaProxies = clashMetaProxies.map((proxy, index) => {
-//     // 如果节点名已经有序号前缀了，也可以先去掉或跳过，这里简单加前缀
-//     const prefix = `${String(index + 1).padStart(2, '0')} `
-//     return {
-//         ...proxy,
-//         name: prefix + proxy.name
-//     }
-// })
+clashMetaProxies = removeUnderscoreFields(clashMetaProxies)
 
 // 插入代理到 YAML 中
-// yaml.proxies = [...clashMetaProxies, ...(yaml.proxies || [])]
-yaml.proxies.unshift(...clashMetaProxies)
+yaml.proxies = [...clashMetaProxies, ...(yaml.proxies || [])]
 
 // 重新转换成 YAML 字符串
 $content = ProxyUtils.yaml.dump(yaml)
